d1052e2e47ea325e316f6bd7b3a55491ba6f90d9
Justin292929 <jubaldesco@gbox.ncf.edu.ph>
2024-11-22 03:51:56 -0800

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
         }}
       >
         <img
-          src="\src\assets\image\photo_2024-11-21_19-17-46.jpg"
+          src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462535496_1558737718337311_7958809534819391584_n.jpg?stp=dst-jpg_s640x640&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=n8onveHmF4QQ7kNvgEiNWtB&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1QEZQlG5iih56XNN7k6z2ogaCU89RUdMNrJsibBdJVMJ8A&oe=6767C9E4"
           alt="Profile"
           style={{
             width: '200px',
@@ -27,7 +27,7 @@ function Home() {
         />
         <h2 style={{ margin: '0', fontSize: '1.5rem', textAlign: 'center' }}>Justin Michael A.Baldesco</h2>
         <p style={{ marginTop: '10px', textAlign: 'center', maxWidth: '600px' }}>
-        Dive in to discover my journey, passions, and the things that inspire me. Feel free to explore and connect there’s so much to share!
+                           Wassupp!!! Welcome to my Page
         </p>
       </div>
 
@@ -42,7 +42,7 @@ function Home() {
       >
         <a href="https://www.facebook.com/profile.php?id=100014371423903" target="_blank" rel="noopener noreferrer">
           <img
-            src="src\assets\image\Beautiful-design-Facebook-logo-social-media-png.png"
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462550714_594188576612781_4704204557448289510_n.jpg?stp=dst-jpg_s480x480&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=XK8Cnw4BeV8Q7kNvgFAjsaH&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1QF4bROneOEwedkt8LcEzwg1FL63idRHS5KXSWI-N6cL_A&oe=6767EFEB"
             alt="Small Profile"
             style={{
               width: '50px',
